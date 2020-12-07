ea97a7c4ece492923eb737d5625231f10f4307e4
YizYah <yyakovson613@gmail.com>
2020-12-07 15:50:50 +0200

    fixes to specs

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 #!/usr/bin/env node
 
-const shell = require('shelljs')
 const codeDir = process.argv[2]
 
-function createCodeBase(codeDir){
-    // console.log(`hello world ${codeDir}`)
-
-    const templateDir = __dirname + '/template'
-    shell.exec(`npx ns-flip generate ${codeDir} -t ${templateDir}`)
-
-}
-
 if (codeDir) {
+    console.log('Welcome to easy-oclif-cli! This process may a few minutes.' +
+                    '  We will download a temporary version of ns-flip, and then of oclif.' +
+                    '  You will have to enter some information at the prompts.')
     const { spawn } = require('child_process')
     const templateDir = __dirname + '/template'
-    const shell = spawn('npx',['ns-flip', 'generate', codeDir, '-t', templateDir], { stdio: 'inherit' })
-    shell.on('close',(code)=>{console.log('[shell] terminated :',code)})
-    // createCodeBase(codeDir)
+    // const shell = spawn('npx',['ns-flip', 'generate', codeDir, '-t', templateDir], { stdio: 'inherit' })
+    const shell = spawn('ns',['generate', codeDir, '-t', templateDir], { stdio: 'inherit' })
+    shell.on('close',(code)=> {
+        if (code === 0) console.log(`Your template was created at ${codeDir}.`)
+    })
 } else {
     console.error('You need to provide a path to the new code base.' +
         '  Usage:\n\teasy-oclif-cli path/to/new/codebase')
