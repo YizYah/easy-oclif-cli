3caaabb4b46fbe60dcd5f01b86bedcb5bceffa61
YizYah <yyakovson613@gmail.com>
2020-12-29 13:51:58 +0200

    new message

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,10 @@ const codeDir = process.argv[2]
 
 if (codeDir) {
     console.log('Welcome to easy-oclif-cli! This process may a few minutes.' +
-                    '  We will download a temporary version of ns-flip, and then of oclif.' +
-                    '  You will have to enter some information at the prompts.')
+                    '  We will download a temporary version of ns-flip, which will install ' +
+                    '  needed packages. You will have to enter some information at the prompts.')
     const { spawn } = require('child_process')
     const templateDir = __dirname + '/../src/template'
-
     const shell = spawn('npx',['ns-flip', 'generate', codeDir, '-t', templateDir], { stdio: 'inherit' })
     shell.on('close',(code)=> {
         if (code === 0) console.log(`Your template was created at ${codeDir}.`)
